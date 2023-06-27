2f8767cffb4c586b51cd0da3a5c3960f5f0bd454
begumkaraoglu <begumkaraoglu93@gmail.com>
2023-06-27 17:17:29 +0300

    v2

--- Miuul.py
+++ Miuul.py
@@ -1,4 +1,12 @@
-def say_hello():
-    print("Merhaba Dünyalı")
+#def say_hello():
+#    print("Merhaba Dünyalı")
+#
+#say_hello()
 
-say_hello()
+import datetime
+
+def say_hello_to():
+    print("Selam Dünyalı Ka-Çı-Rıl-Dın!!")
+    print("Kaçırılma tarihin tam olaraaaak:", datetime.datetime.now())
+
+say_hello_to()
